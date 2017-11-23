e7fcd55bf11c76e7d1d356c9693c8fd30f96b73f
Alex Klein <S.Alex.Klein@gmail.com>
2017-11-22 22:23:33 -0700

    No edge value instead of 0 check. Added mst overlay output.

--- DotGen.h
+++ DotGen.h
@@ -10,12 +10,30 @@
 
 using namespace std;
 
-void matrixOutput(int **g, int v,  const char * filename) {
-    FILE * outputFile = fopen(filename, "w");
+void matrixOutput(int **g, int v, const char *filename) {
+    FILE *outputFile = fopen(filename, "w");
     fprintf(outputFile, "strict graph {\n");
     for (int i = 0; i < v; i++) {
         for (int j = 0; j < i; j++) {
-            if (g[i][j] > 0) {
+            if (g[i][j] != NO_EDGE) {
+                fprintf(outputFile, "  %d -- %d [label=%d]\n", i, j, g[i][j]);
+            }
+        }
+    }
+
+    fprintf(outputFile, "}\n");
+
+    fclose(outputFile);
+}
+
+void matrixMstOutput(int **g, int **mst, int v, const char *filename) {
+    FILE *outputFile = fopen(filename, "w");
+    fprintf(outputFile, "strict graph {\n");
+    for (int i = 0; i < v; i++) {
+        for (int j = 0; j < i; j++) {
+            if (mst[i][j] != NO_EDGE) {
+                fprintf(outputFile, "  %d -- %d [label=%d,color=red,penwidth=3.0]\n", i, j, g[i][j]);
+            } else if (g[i][j] != NO_EDGE) {
                 fprintf(outputFile, "  %d -- %d [label=%d]\n", i, j, g[i][j]);
             }
         }
