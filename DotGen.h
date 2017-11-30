89f077e2d6f61cd89cf9212edd8999376100cd25
Alex Klein <S.Alex.Klein@gmail.com>
2017-11-30 10:02:58 -0700

    Included each vertex in graph to ensure everything was included. Changed j iterations to have easier to scan .dot files.

--- DotGen.h
+++ DotGen.h
@@ -13,8 +13,14 @@ using namespace std;
 void matrixOutput(int **g, int v, const char *filename) {
     FILE *outputFile = fopen(filename, "w");
     fprintf(outputFile, "strict graph {\n");
+
+    // Include individual vertices in case of vertex with no edges
     for (int i = 0; i < v; i++) {
-        for (int j = 0; j < i; j++) {
+        fprintf(outputFile, "  %d\n", i);
+    }
+
+    for (int i = 0; i < v; i++) {
+        for (int j = i+1; j < v; j++) {
             if (g[i][j] != NO_EDGE) {
                 fprintf(outputFile, "  %d -- %d [label=%d]\n", i, j, g[i][j]);
             }
@@ -30,7 +36,7 @@ void matrixMstOutput(int **g, int **mst, int v, const char *filename) {
     FILE *outputFile = fopen(filename, "w");
     fprintf(outputFile, "strict graph {\n");
     for (int i = 0; i < v; i++) {
-        for (int j = 0; j < i; j++) {
+        for (int j = i+1; j < v; j++) {
             if (mst[i][j] != NO_EDGE) {
                 fprintf(outputFile, "  %d -- %d [label=%d,color=red,penwidth=3.0]\n", i, j, g[i][j]);
             } else if (g[i][j] != NO_EDGE) {
