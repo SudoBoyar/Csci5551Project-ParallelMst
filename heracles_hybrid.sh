2b78b27a348e508bb3a1ebb73c7731de6c6a3d04
Alex Klein <S.Alex.Klein@gmail.com>
2017-12-07 22:49:55 -0700

    Fixed repo copy of the heracles hybrid run file.

--- heracles_hybrid.sh
+++ heracles_hybrid.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-mpirun --display-map -hostfile heracles_hybrid_hosts ~/csc5551/project/mpi 480 1
-mpirun --display-map -hostfile heracles_hybrid_hosts ~/csc5551/project/mpi 480 1
-mpirun --display-map -hostfile heracles_hybrid_hosts ~/csc5551/project/mpi 480 1
-mpirun --display-map -hostfile heracles_hybrid_hosts ~/csc5551/project/mpi 4800 1
-mpirun --display-map -hostfile heracles_hybrid_hosts ~/csc5551/project/mpi 4800 1
-mpirun --display-map -hostfile heracles_hybrid_hosts ~/csc5551/project/mpi 4800 1
-mpirun --display-map -hostfile heracles_hybrid_hosts ~/csc5551/project/mpi 48000 1
-mpirun --display-map -hostfile heracles_hybrid_hosts ~/csc5551/project/mpi 48000 1
-mpirun --display-map -hostfile heracles_hybrid_hosts ~/csc5551/project/mpi 48000 1
+mpirun --display-map -hostfile heracles_hybrid_hosts ~/csc5551/project/hybrid 480 1
+mpirun --display-map -hostfile heracles_hybrid_hosts ~/csc5551/project/hybrid 480 1
+mpirun --display-map -hostfile heracles_hybrid_hosts ~/csc5551/project/hybrid 480 1
+mpirun --display-map -hostfile heracles_hybrid_hosts ~/csc5551/project/hybrid 4800 1
+mpirun --display-map -hostfile heracles_hybrid_hosts ~/csc5551/project/hybrid 4800 1
+mpirun --display-map -hostfile heracles_hybrid_hosts ~/csc5551/project/hybrid 4800 1
+mpirun --display-map -hostfile heracles_hybrid_hosts ~/csc5551/project/hybrid 48000 1
+mpirun --display-map -hostfile heracles_hybrid_hosts ~/csc5551/project/hybrid 48000 1
+mpirun --display-map -hostfile heracles_hybrid_hosts ~/csc5551/project/hybrid 48000 1
